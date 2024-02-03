dfbf2354af4f8dbdbe31566ec6576c3106ce5b17
sorawat <s.tanthikun802@mybvc.ca>
2024-02-03 15:33:38 -0700

    build: remove required repo

--- internal/service/cartService/cart.go
+++ internal/service/cartService/cart.go
@@ -1,18 +1,14 @@
 package cartservice
 
 import (
-	"github.com/CLCM3102-Ice-Cream-Shop/backend-payment-service/internal/adaptor/repositories/database"
 	"github.com/CLCM3102-Ice-Cream-Shop/backend-payment-service/internal/service"
 )
 
 type cartService struct {
-	cartRepo database.CartRepository
 }
 
-func New(cartRepo database.CartRepository) service.CartService {
-	return cartService{
-		cartRepo: cartRepo,
-	}
+func New() service.CartService {
+	return cartService{}
 }
 
 const ()
