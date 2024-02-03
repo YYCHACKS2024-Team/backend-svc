80e7c74095d6ac8f37efb155b021ad3232414ec6
sorawat <s.tanthikun802@mybvc.ca>
2024-02-03 15:41:38 -0700

    build: comment unused code

--- internal/adaptor/repositories/database/cartDB/cart.go
+++ internal/adaptor/repositories/database/cartDB/cart.go
@@ -1,90 +1,83 @@
 package cartdb
 
-import (
-	"time"
+// type repository struct {
+// 	db *gorm.DB
+// }
 
-	"github.com/CLCM3102-Ice-Cream-Shop/backend-payment-service/internal/models"
-	"gorm.io/gorm"
-)
+// func New(db *gorm.DB) repository {
+// 	return repository{
+// 		db: db,
+// 	}
+// }
 
-type repository struct {
-	db *gorm.DB
-}
+// func (repo repository) Insert(cart models.Cart) error {
 
-func New(db *gorm.DB) repository {
-	return repository{
-		db: db,
-	}
-}
+// 	if tx := repo.db.Create(&cart); tx.Error != nil {
+// 		return tx.Error
+// 	}
 
-func (repo repository) Insert(cart models.Cart) error {
+// 	return nil
+// }
 
-	if tx := repo.db.Create(&cart); tx.Error != nil {
-		return tx.Error
-	}
+// func (repo repository) Find(cartId string, no int) (models.Cart, error) {
 
-	return nil
-}
+// 	var cart models.Cart
+// 	if tx := repo.db.Where(models.Cart{CartId: cartId, No: no}).First(&cart); tx.Error != nil {
+// 		return models.Cart{}, tx.Error
+// 	}
 
-func (repo repository) Find(cartId string, no int) (models.Cart, error) {
+// 	return cart, nil
+// }
 
-	var cart models.Cart
-	if tx := repo.db.Where(models.Cart{CartId: cartId, No: no}).First(&cart); tx.Error != nil {
-		return models.Cart{}, tx.Error
-	}
+// func (repo repository) FindById(cartId string) ([]models.Cart, error) {
 
-	return cart, nil
-}
+// 	var carts []models.Cart
 
-func (repo repository) FindById(cartId string) ([]models.Cart, error) {
+// 	if tx := repo.db.Where(models.Cart{CartId: cartId}).Find(&carts); tx.Error != nil {
+// 		return nil, tx.Error
+// 	}
 
-	var carts []models.Cart
+// 	return carts, nil
+// }
 
-	if tx := repo.db.Where(models.Cart{CartId: cartId}).Find(&carts); tx.Error != nil {
-		return nil, tx.Error
-	}
+// func (repo repository) FindByCustomerIdAndStatus(customerId string, status string) ([]models.Cart, error) {
 
-	return carts, nil
-}
+// 	var carts []models.Cart
+// 	tx := repo.db.Where("customer_id = ? AND status = ?", customerId, status).Find(&carts)
+// 	if tx.Error != nil {
+// 		return nil, tx.Error
+// 	}
 
-func (repo repository) FindByCustomerIdAndStatus(customerId string, status string) ([]models.Cart, error) {
+// 	return carts, nil
+// }
 
-	var carts []models.Cart
-	tx := repo.db.Where("customer_id = ? AND status = ?", customerId, status).Find(&carts)
-	if tx.Error != nil {
-		return nil, tx.Error
-	}
+// func (repo repository) UpdateStatus(id string, no int, status string) error {
 
-	return carts, nil
-}
+// 	if tx := repo.db.Model(&models.Cart{}).Where(models.Cart{CartId: id, No: no}).Update("status", status); tx.Error != nil {
+// 		return tx.Error
+// 	}
 
-func (repo repository) UpdateStatus(id string, no int, status string) error {
+// 	return nil
+// }
 
-	if tx := repo.db.Model(&models.Cart{}).Where(models.Cart{CartId: id, No: no}).Update("status", status); tx.Error != nil {
-		return tx.Error
-	}
+// func (repo repository) UpdateStatusById(id string, status string) error {
 
-	return nil
-}
+// 	if tx := repo.db.Model(&models.Cart{}).Where(models.Cart{CartId: id}).Updates(models.Cart{Status: status}); tx.Error != nil {
+// 		return tx.Error
+// 	}
 
-func (repo repository) UpdateStatusById(id string, status string) error {
+// 	return nil
+// }
 
-	if tx := repo.db.Model(&models.Cart{}).Where(models.Cart{CartId: id}).Updates(models.Cart{Status: status}); tx.Error != nil {
-		return tx.Error
-	}
+// func (repo repository) GetCartByDateMonth(date time.Time) ([]models.Cart, error) {
 
-	return nil
-}
+// 	startOfMonth := time.Date(date.Year(), date.Month(), 1, 0, 0, 0, 0, time.Local)
+// 	endOfMonth := startOfMonth.AddDate(0, 1, 0).Add(-time.Second)
 
-func (repo repository) GetCartByDateMonth(date time.Time) ([]models.Cart, error) {
+// 	var carts []models.Cart
+// 	if err := repo.db.Where("date BETWEEN ? AND ?", startOfMonth, endOfMonth).Find(&carts).Error; err != nil {
+// 		return nil, err
+// 	}
 
-	startOfMonth := time.Date(date.Year(), date.Month(), 1, 0, 0, 0, 0, time.Local)
-	endOfMonth := startOfMonth.AddDate(0, 1, 0).Add(-time.Second)
-
-	var carts []models.Cart
-	if err := repo.db.Where("date BETWEEN ? AND ?", startOfMonth, endOfMonth).Find(&carts).Error; err != nil {
-		return nil, err
-	}
-
-	return carts, nil
-}
+// 	return carts, nil
+// }
