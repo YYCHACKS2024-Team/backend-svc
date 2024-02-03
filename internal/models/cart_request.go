bebaf25428bd32f840f296dab4bae8d308be9bd5
sorawat <s.tanthikun802@mybvc.ca>
2024-02-03 15:31:31 -0700

    build: remove old code

--- internal/models/cart_request.go
+++ internal/models/cart_request.go
@@ -1,7 +1,5 @@
 package models
 
-import "github.com/shopspring/decimal"
-
 type AddToCartRequest struct {
 	CustomerId        string `json:"customer_id,omitempty"`
 	MenuId            string `json:"menu_id,omitempty"`
@@ -18,43 +16,4 @@ type AddToCartResponse struct {
 type AddToCartResponseBody struct {
 	CartId string `json:"cart_id,omitempty"`
 	No     int    `json:"no,omitempty"`
-}
-
-type GetCartByCustomerIdResponse struct {
-	CommonResponse
-	Data GetCartByCustomerIdResponseBody `json:"data,omitempty"`
-}
-
-type GetCartByCustomerIdResponseBody struct {
-	CartId     string                          `json:"cart_id,omitempty"`
-	CartDetail []GetCartByCustomerIdCartDetail `json:"cart_detail,omitempty"`
-}
-type GetCartByCustomerIdCartDetail struct {
-	No                int             `json:"no"`
-	CustomerId        string          `json:"customer_id"`
-	Date              string          `json:"date"`
-	MenuId            string          `json:"menu_id"`
-	MenuName          string          `json:"menu_name"`
-	Quantity          int             `json:"quantity"`
-	Status            string          `json:"status"`
-	Properties        string          `json:"properties,omitempty"`
-	AdditionalRequest string          `json:"additional_request,omitempty"`
-	Price             decimal.Decimal `json:"price"`
-}
-
-type GetCartByIdResponse struct {
-	CommonResponse
-	Data []GetCartByIdResponseBody `json:"data,omitempty"`
-}
-
-type GetCartByIdResponseBody struct {
-	CartId            string `json:"cart_id"`
-	No                int    `json:"no"`
-	CustomerId        string `json:"customer_id"`
-	Date              string `json:"date"`
-	MenuId            string `json:"menu_id"`
-	Quantity          int    `json:"quantity"`
-	Status            string `json:"status"`
-	Properties        string `json:"properties,omitempty"`
-	AdditionalRequest string `json:"additional_request,omitempty"`
 }
